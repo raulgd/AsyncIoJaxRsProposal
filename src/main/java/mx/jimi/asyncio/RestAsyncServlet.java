0eb312d4be8bdb23016f8c9328d7cd0720880602
Guerrero, Raul D <raul.d.guerrero@delphi.com>
2014-12-17 07:21:38 -0600

    Modified the order of calls on the servlet to assign the non-blocking event handlers implemented in the resource before the async I/O begins to execute.

--- src/main/java/mx/jimi/asyncio/RestAsyncServlet.java
+++ src/main/java/mx/jimi/asyncio/RestAsyncServlet.java
@@ -36,18 +36,21 @@ public class RestAsyncServlet extends HttpServlet
 		//initialize based on whatever is needed from the JAX-RS application as well, for now just the basics
 		RestAsyncContext ctx = new RestAsyncContext(context, request, response);
 
+		//the rest async context object (ctx) has to be injected automagically into the REST resource using the Async
+		//annotation in the resource method so that we assign the interfaces implemented in the resource to the context events
+
+
+		//here we just assign the read and write listeners to the RestAsyncContext to begin executing the async I/O
+		//processing, but if it wasn't a servlet, here you could assign whatever non-blocking HTTP I/O framework that
+		//implements the Async context interfaces, like something on top of netty or grizzly
 		request.getInputStream().setReadListener(ctx);
 		response.getOutputStream().setWriteListener(ctx);
-
-		//after here, the rest async context object (ctx) has to be injected automagically to the REST resource using the
-		//Async annotation in the REST resource
 	}
 
 	/**
 	 * Add support for all verbs and whatever, this is also a JAX-RS implementation thing, for this sample just GET and
 	 * PUT
 	 */
-
 	/**
 	 * Send the GET verb to processRequest
 	 *
